5528f48d527c60fffa368b5394d87fc342f5b7c5
Stefan Wille <github@stefanwille.com>
2018-06-06 15:48:40 +0200

    Add axios

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import "./App.css";
 import ContactForm from "./ContactForm";
 
 class App extends Component {
+  handleFormSubmit = formValues => {
+    axios.post("localhost:3000/something", formValues);
+  };
+
   render() {
     return (
       <div className="App">
-        <ContactForm />
+        <ContactForm onSubmit={this.handleFormSubmit} />
       </div>
     );
   }
