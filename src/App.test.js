428f61073810d6fa3f1e681aa44ec2b0174290cf
Stefan Wille <github@stefanwille.com>
2018-06-06 13:14:49 +0200

    Improve test api

--- src/App.test.js
+++ src/App.test.js
@@ -2,35 +2,57 @@ import React from "react";
 import { mount } from "enzyme";
 import App from "./App";
 
-const updateInput = (wrapper, instance, newValue) => {
-  const input = wrapper.find(instance);
+const selectRadioButton = (wrapper, fieldName, newValue) => {
+  const selector = `[field="${fieldName}"] input[value="${newValue}"]`;
+  const input = wrapper.find(selector);
+  if (input.length === 0) {
+    throw new Error(`Could not find radio button for selector ${selector}`);
+  }
   input.simulate("change", {
     target: { value: newValue }
   });
 
-  return wrapper.find(instance);
+  return wrapper.find(selector);
+};
+
+const enterText = (wrapper, fieldName, newValue) => {
+  const selector = `[field="${fieldName}"] input`;
+  const input = wrapper.find(selector);
+  if (input.length === 0) {
+    throw new Error(`Could not find textarea for selector ${selector}`);
+  }
+  input.simulate("change", {
+    target: { value: newValue }
+  });
+
+  return wrapper.find(selector);
+};
+
+const enterTextarea = (wrapper, fieldName, newValue) => {
+  const selector = `[field="${fieldName}"] textarea`;
+  const input = wrapper.find(selector);
+  if (input.length === 0) {
+    throw new Error(`Could not find textarea for selector ${selector}`);
+  }
+  input.simulate("change", {
+    target: { value: newValue }
+  });
+
+  return wrapper.find(selector);
 };
 
 test("user can fill out and submit to API", () => {
   const wrapper = mount(<App />);
 
-  const firstName = updateInput(
-    wrapper,
-    '[field="firstName"] input',
-    "Esteban"
-  );
+  const firstName = enterText(wrapper, "firstName", "Esteban");
   expect(firstName.props().value).toBe("Esteban");
 
-  const lastName = updateInput(wrapper, '[field="lastName"] input', "Caldron");
+  const lastName = enterText(wrapper, "lastName", "Caldron");
   expect(lastName.props().value).toBe("Caldron");
 
-  const gender = updateInput(
-    wrapper,
-    '[field="gender"] input[value="male"]',
-    "male"
-  );
+  const gender = selectRadioButton(wrapper, "gender", "male");
   expect(gender.props().value).toBe("male");
 
-  const bio = updateInput(wrapper, '[field="bio"] textarea', "My bio");
+  const bio = enterTextarea(wrapper, "bio", "My bio");
   expect(bio.props().value).toBe("My bio");
 });
